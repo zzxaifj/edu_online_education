0fe60417ef3601672eefe67eb1b44fcafd5e802c
zzxaifj <1179278531@qq.com>
2020-06-07 08:40:25 +0800

    #1、讲师模块基本增删改查功能完成  2、新建前端查询类 TQ
    Signed-off-by: zzxaifj <1179278531@qq.com>

--- service/service_edu/src/main/java/com/edu/eduservice/controller/EduTeacherController.java
+++ service/service_edu/src/main/java/com/edu/eduservice/controller/EduTeacherController.java
@@ -1,14 +1,16 @@
 package com.edu.eduservice.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.edu.eduservice.entity.EduTeacher;
+import com.edu.eduservice.entity.vo.TeacherQuery;
 import com.edu.eduservice.service.EduTeacherService;
-import com.edu.servicebase.handler.BusinessException;
 import com.edu.tools.R;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 /**
  * <p>
@@ -19,33 +21,115 @@ import java.util.List;
  * @since 2020-06-01
  */
 @RestController
-@RequestMapping("/eduservice/edu-teacher")
+@RequestMapping("/eduservice/teacher")
+@CrossOrigin
 public class EduTeacherController {
     @Autowired
     private EduTeacherService eduTeacherService;
 
     /**
-    * @Description 获取所有数据
+    * @Description 获取所有数据<分页></分页>
     * @Author zhengzhaoxiang
     * @Date 2020/6/2 15:27
     * @Param []
     * @Return void
     */
-    @GetMapping("findAll")
-    public R findAll(){
-        List<EduTeacher> list = eduTeacherService.list(null);
-        return R.ok().data("items",list);
+    @PostMapping("pageTeacherCondition/{current}/{limit}")
+    public R pageTeacherCondition( @PathVariable long current,
+                                   @PathVariable long limit,
+                                  @RequestBody(required = false) TeacherQuery teacherQuery){
+        Page<EduTeacher> pageTeacher = new Page<>(current,limit);
+        //构建条件
+        QueryWrapper<EduTeacher> wrapper = new QueryWrapper<>();
+        // mybatis学过 动态sql
+        if(teacherQuery != null){
+
+            String name = teacherQuery.getName();
+            Integer level = teacherQuery.getLevel();
+            String begin = teacherQuery.getBegin();
+            String end = teacherQuery.getEnd();
+            //判断条件值是否为空，如果不为空拼接条件
+            if(!StringUtils.isEmpty(name)) {
+                //构建条件
+                wrapper.like("name",name);
+            }
+            if(!StringUtils.isEmpty(level)) {
+                wrapper.eq("level",level);
+            }
+            if(!StringUtils.isEmpty(begin)) {
+                wrapper.ge("gmt_create",begin);
+            }
+            if(!StringUtils.isEmpty(end)) {
+                wrapper.le("gmt_create",end);
+            }
+        }
+        //排序
+        wrapper.orderByDesc("gmt_create");
+        //分页查找
+        eduTeacherService.page(pageTeacher,wrapper);
+        //List<EduTeacher> list = eduTeacherService.list(null);
+        //获取总记录数 和 记录
+        return R.ok().data("rows",pageTeacher.getRecords()).data("total",pageTeacher.getTotal() );
     }
 
-    @PostMapping("save")
-    public void insert(@RequestBody EduTeacher eduTeacher){
-        eduTeacherService.save(eduTeacher);
+    /**
+    * @Description 保存数据
+    * @Author zhengzhaoxiang
+    * @Date 2020/6/6 13:27
+    * @Param [eduTeacher]
+    * @Return com.edu.tools.R
+    */
+    @PostMapping("addTeacher")
+    public R insert(@RequestBody EduTeacher eduTeacher){
+        boolean save = eduTeacherService.save(eduTeacher);
+        if(save){
+            return R.ok();
+        }else {
+            return R.error();
+        }
     }
 
+    /**
+    * @Description 删除数据
+    * @Author zhengzhaoxiang
+    * @Date 2020/6/6 13:28
+    * @Param [id]
+    * @Return com.edu.tools.R
+    */
     @DeleteMapping("{id}")
-    public boolean removeTeacher(@PathVariable String id){
+    public R removeTeacher(@PathVariable String id){
         boolean b = eduTeacherService.removeById(id);
-        return b;
+        if(b){
+          return R.ok();
+        } else {
+            return R.error();
+        }
+    }
+
+    /**
+    * @Description 根据id获取teacher信息
+    * @Author zhengzhaoxiang
+    * @Date 2020/6/6 13:31
+    * @Param [id]
+    * @Return com.edu.tools.R
+    */
+    @GetMapping("getTeacher/{id}")
+    public R getById(@PathVariable String id){
+        if(id != null){
+            EduTeacher teacher = eduTeacherService.getById(id);
+            return R.ok().data("teacher",teacher);
+        }
+        return R.error().message("id 不能为空");
+    }
+
+    @PutMapping("updateTeacher")
+    public R update(@RequestBody EduTeacher teacher){
+        boolean update = eduTeacherService.updateById(teacher);
+        if(update){
+            return R.ok();
+        }else{
+            return R.error();
+        }
     }
 
 }
